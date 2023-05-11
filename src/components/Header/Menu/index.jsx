af6132a7f783630be6d140bba0be77dd624a02c9
Zehra <aydin.fzehra@gmail.com>
2023-05-11 14:18:13 +0300

    style: update menu

--- src/components/Header/Menu/index.jsx
+++ src/components/Header/Menu/index.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import More from "./More";
 
 export default function Menu() {
   return (
     <div className="container w-5/6 sm:w-full mx-auto">
-      <ul className="hidden sm:flex justify-between py-3 text-sm">
+      <ul className="hidden sm:flex justify-between items-center py-3 text-sm">
         <li>Menu Item</li>
         <li>Menu Item</li>
         <li>Menu Item</li>
         <li>Menu Item</li>
         <li>Menu Item</li>
         <li>Menu Item</li>
-        <li className="hidden sm:flex lg:hidden">More</li>
+        <li className="hidden sm:flex lg:hidden">
+          <More/>
+        </li>
         <li className="sm:hidden lg:flex">Menu Item</li>
         <li className="sm:hidden lg:flex">Menu Item</li>
         <li className="sm:hidden lg:flex">Menu Item</li>
