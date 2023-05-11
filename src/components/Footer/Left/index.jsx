91067d3edf5b717b79c3af497eba78e966e9ebab
Zehra <aydin.fzehra@gmail.com>
2023-05-11 14:15:05 +0300

    style: update footer and add logo

--- src/components/Footer/Left/index.jsx
+++ src/components/Footer/Left/index.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 export default function Left() {
   return (
-    <div class="w-full sm:w-1/2 flex flex-col gap-y-5 text-white py-10 ">
+    <div className="w-full sm:w-1/2 flex flex-col gap-y-5 text-white py-10 ">
       <div>
-        <h4 className="text-xl md:text-2xl font-bold">NTT DATA</h4>
-        <p className="text-xs leading-none sm:text-sm">Trusted Global Innovator</p>
+        <img className="h-10 bg-white p-1" src="/logo.svg" alt="" />
+        <p className="text-xs leading-none sm:text-sm">
+          Trusted Global Innovator
+        </p>
       </div>
 
       <p className="hidden sm:flex">
@@ -16,12 +18,12 @@ export default function Left() {
       <div className="flex h-6 sm:h-10">
         <input
           type="text"
-          class="bg-white w-1/2 px-3 outline-none text-xs sm:text-base"
+          className="bg-white w-1/2 px-3 outline-none text-xs sm:text-base"
           placeholder="Email address"
         />
         <button
           type="submit"
-          class="bg-blue-950 inline-block px-6 text-xs font-normal uppercase "
+          className="bg-blue-950 inline-block px-6 text-xs font-normal uppercase "
         >
           Sign Up
         </button>
